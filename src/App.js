b0c33fd80d8f06538d748eb28aa5d2c87783b166
siva <sivasankar@troopr.io>
2020-09-02 08:12:19 +0530

    adde header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homePage.component"
 import ShopPage from "./pages/shop/shop.component.jsx"
+import Header from "./components/Header/header.jsx"
 import {Switch,Route} from "react-router-dom"
-
-const HatsPage=()=>(<div>Hello</div>)
-
 function App() {
   return (
     <div>
-     
+     <Header />
       <Switch> 
    <Route exact path="/" component={HomePage} /> 
      <Route exact path="/shop" component={ShopPage} /> 
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./header.scss"
+import {Link} from "react-router-dom"
+import {ReactComponent as Logo} from "../../assets/crown.svg"
+const Header=()=>(
+<div className="header">
+<Link to="/" >
+<Logo className="logo" />
+</Link>
+
+<div className="options">
+    <Link className='option' to='/shop'>Shop</Link>
+    <Link className='option' to='/contact'>Contact</Link>
+    {/* <Link className='option' to='/shop'>Shop</Link>
+    <Link className='option' to='/shop'>Shop</Link> */}
+</div>
+</div>
+)
+export default Header
+
